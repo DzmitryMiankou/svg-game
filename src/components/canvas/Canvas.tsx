29e0b3d7970b60e70e951f5dcfcdf129fe3c73c0
DzmitryMiankou <gmiankou@gmail.com>
2023-10-27 19:30:29 +0300

    ok

--- src/components/canvas/Canvas.tsx
+++ src/components/canvas/Canvas.tsx
@@ -1,5 +1,11 @@
 import React from "react";
 import { StateSizeCanvasType } from "../../App";
+import styled from "styled-components";
+
+const SVG = styled.svg`
+  width: 100%;
+  background-color: #f2f8ab;
+`;
 
 const enum KeyType {
   ArrowRight = "ArrowRight",
@@ -8,17 +14,29 @@ const enum KeyType {
   ArrowDown = "ArrowDown",
 }
 
-const heroProp = { size: 50, step: 5 };
+const heroProp = { size: 50, step: 10 };
 
 const Canvas: React.FC<{ get: StateSizeCanvasType }> = ({ get }) => {
   const [key, setKey] = React.useState<string>();
   const [koordX, setCoordX] = React.useState<number>(0);
   const [koordY, setCoordY] = React.useState<number>(0);
-  const ref = React.useRef<HTMLCanvasElement>(null);
 
   const { height, width } = get;
 
-  const switchKays = React.useCallback((key: string) => {
+  const labProp = React.useMemo(() => {
+    return [
+      { x: width / 2, y: 50, width: 50, height: height - 100, fill: "#a6b522" },
+      {
+        x: width / 2 - 120,
+        y: 0,
+        width: 50,
+        height: height - 50,
+        fill: "#a6b522",
+      },
+    ];
+  }, [height, width]);
+
+  const switchKeys = React.useCallback((key: string) => {
     switch (key) {
       case KeyType.ArrowRight:
         setKey(KeyType.ArrowRight);
@@ -44,45 +62,58 @@ const Canvas: React.FC<{ get: StateSizeCanvasType }> = ({ get }) => {
   React.useEffect(() => {
     const keyDownHandler = (event: KeyboardEvent) => {
       const key = event.key;
-      return switchKays(key);
+      return switchKeys(key);
     };
     window.addEventListener("keydown", keyDownHandler);
     return window.removeEventListener("keyup", keyDownHandler);
-  }, [switchKays]);
+  }, [switchKeys]);
 
   React.useEffect(() => {
-    if (ref.current) {
-      const canvas = ref.current;
-      const context = canvas.getContext("2d");
-      if (context) startDrow(context);
-    }
+    if (koordX < 0) setCoordX(0);
+    if (koordY < 0) setCoordY(0);
+    if (koordX > width - heroProp.size) setCoordX(width - heroProp.size);
+    if (koordY > height - heroProp.size) setCoordY(height - heroProp.size);
 
-    function startDrow(context: CanvasRenderingContext2D) {
-      if (koordX < 0) return setCoordX(0);
-      if (koordY < 0) return setCoordY(0);
-      if (koordX > width - 50) return setCoordX(width - 50);
-      if (koordY > height - 50) return setCoordY(height - 50);
+    function colisObj(hero: number, x: number, y: number, height: number) {
       if (
-        koordX < 500 + 50 &&
-        koordX + 50 > 500 &&
-        koordY < 50 + height - 100 &&
-        koordY + 50 > 50
+        koordX < x + hero &&
+        koordX + hero > x &&
+        koordY < y + height &&
+        koordY + hero > y
       ) {
-        if (key === KeyType.ArrowRight) return setCoordX(500 - 50);
-        if (key === KeyType.ArrowLeft) return setCoordX(500 + 50);
-        if (key === KeyType.ArrowDown) return setCoordY(50 - 50);
-        if (key === KeyType.ArrowUp) return setCoordY(height - 100 + 50);
+        if (key === KeyType.ArrowRight) setCoordX(x - hero);
+        if (key === KeyType.ArrowLeft) setCoordX(x + hero);
+        if (key === KeyType.ArrowDown) setCoordY(hero - y);
+        if (key === KeyType.ArrowUp) setCoordY(height + hero);
+        return;
       }
-      context.canvas.width = width;
-      context.canvas.height = height;
-      context.fillStyle = "red";
-      context.fillRect(koordX, koordY, heroProp.size, heroProp.size);
-      context.fillStyle = "rgba(0, 0, 200, 0.5)";
-      context.fillRect(500, 50, 50, height - 100);
     }
-  }, [koordX, height, width, koordY, key]);
+    labProp.forEach(({ x, y, height }) => {
+      return colisObj(heroProp.size, x, y, height);
+    });
+  }, [koordX, height, width, koordY, key, labProp]);
 
-  return <canvas ref={ref} />;
+  return (
+    <SVG
+      x="0px"
+      y="0px"
+      viewBox={`0 0 ${width} ${height}`}
+      enableBackground={`new 0 0 ${width} ${height}`}
+    >
+      <rect
+        x={koordX}
+        y={koordY}
+        fill="#22B573"
+        width={heroProp.size}
+        height={heroProp.size}
+      />
+      <g>
+        {labProp.map((prop, i) => (
+          <rect key={i} {...prop} />
+        ))}
+      </g>
+    </SVG>
+  );
 };
 
 export default Canvas;
